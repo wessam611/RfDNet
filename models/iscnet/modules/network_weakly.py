94a2336b0c0cc19064f15f12439ae7494fe33083
Jonathan Schmidt <jonathan.schmidt@tum.de>
2022-01-20 23:06:13 +0100

    Apply autopep8 style

--- models/iscnet/modules/network_weakly.py
+++ models/iscnet/modules/network_weakly.py
@@ -14,24 +14,25 @@ from net_utils.libs import flip_axis_to_depth, extract_pc_in_box3d, flip_axis_to
 from torch import optim
 from models.loss import chamfer_func
 from net_utils.box_util import get_3d_box
-
+from external.group_loss import data_utility
 
 from .network import ISCNet
+
 
 @METHODS.register_module
 class ISCNet_WEAK(BaseNetwork):
     def __init__(self, cfg):
-        '''
+        """
         load submodules for the network.
         :param config: customized configurations.
-        '''
+        """
         super(BaseNetwork, self).__init__()
         self.cfg = cfg
 
         phase_names = []
         if cfg.config[cfg.config['mode']]['phase'] in ['prior']:
             phase_names += ['completion', 'class_encode']
-                
+
         if (not cfg.config['model']) or (not phase_names):
             cfg.log_string('No submodule found. Please check the phase name and model definition.')
             raise ModuleNotFoundError('No submodule found. Please check the phase name and model definition.')
@@ -55,50 +56,50 @@ class ISCNet_WEAK(BaseNetwork):
 
         '''freeze submodules or not'''
         self.freeze_modules(cfg)
+
     # def sample_for_prior(self, inputs, sample_size):
     #     # temporary naive implementation should be either random
     #     # or sampled by removing some chungs of the input pC so it has holes
     #     # instead of being evenly distributed
     #     return inputs[:, 0:sample_size]
     def forward(self, data, export_shape=False):
-        '''
+        """
         Forward pass of the network
         :param data (dict): contains the data for training.
         :param export_shape: if output shape voxels for visualization
         :return: end_points: dict
-        '''
+        """
         if self.cfg.config[self.cfg.config['mode']]['phase'] == 'prior':
             pc = data['object_pointcloud']
-            input_points = pc #self.sample_for_prior(pc, 256) # not sure where the no. of points per object is set
+            input_points = pc  # self.sample_for_prior(pc, 256) # not sure where the no. of points per object is set
             logits, features_for_completion = self.class_encode(input_points)
             completion_loss, shape_example = self.completion.compute_loss(features_for_completion,
-                                                                        data['object_points'], # just a 3D grid
-                                                                        data['object_occupancies'], # Labels in out
-                                                                        data['label'], # class labels for ShapeNet
-                                                                        export_shape) 
-            return logits, features_for_completion, completion_loss, shape_example # end_points,  BATCH_PROPOSAL_IDs removed
+                                                                          data['object_points'],  # just a 3D grid
+                                                                          data['object_occupancies'],  # Labels in out
+                                                                          data['label'],  # class labels for ShapeNet
+                                                                          export_shape)
+            # NOTE: end_points, BATCH_PROPOSAL_IDs removed
+            return logits, features_for_completion, completion_loss, shape_example
         else:
             pass
             # not yet decided
             # return super(ISCNet_WEAK, self).forward(data, export_shape)
 
     def loss(self, est_data, gt_data):
-        '''
+        """
         calculate loss of est_out given gt_out.
-        '''
-        total_loss = {}
+        """
         # if self.cfg.config[self.cfg.config['mode']]['phase'] == 'prior':
         completion_loss = self.completion_loss(est_data[2])
+
         class_loss = self.class_encode_loss(est_data, gt_data)
         total_loss = {'completion_loss': completion_loss.item(),
-                        'class_loss':class_loss.item()}
-        total_loss['total'] = class_loss + completion_loss
+                      'class_loss': class_loss.item(),
+                      'total': class_loss + completion_loss}
         return total_loss
-    
+
     def compute_metrics(self, est_data, gt_data):
         """
         """
-        metrics = {}
-        metrics['cls_acc_met'] = self.cls_acc_metric(est_data, gt_data)
+        metrics = {'cls_acc_met': self.cls_acc_metric(est_data, gt_data)}
         return metrics
-        
